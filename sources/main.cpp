1ba142f738283e07d618db4445ff173f76733e46
Oscar Hultén <oshulten@gmail.com>
2024-10-28 18:19:18 +0100

    feat: change build to c++

--- sources/main.cpp
+++ sources/main.cpp
@@ -10,7 +10,7 @@ int main(void)
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
     SetTargetFPS(60);
 
-    Texture2D texture = LoadTexture(ASSETS_PATH"test.png"); // Check README.md for how this works
+    // Texture2D texture = LoadTexture(ASSETS_PATH"test.png"); // Check README.md for how this works
 
     while (!WindowShouldClose())
     {
@@ -18,13 +18,13 @@ int main(void)
 
         ClearBackground(RAYWHITE);
 
-        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
-        DrawTexture(texture, texture_x, texture_y, WHITE);
+        // const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
+        // const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
+        // DrawTexture(texture, texture_x, texture_y, WHITE);
 
         const char* text = "OMG! IT WORKS!";
         const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
+        // DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
 
         EndDrawing();
     }
