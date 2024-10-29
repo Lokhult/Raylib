e5c6f76262ed66d6cd5d5651fb8198438c88c12a
Oscar Hultén <oshulten@gmail.com>
2024-10-29 21:23:10 +0100

    feat: got linking to source files from test to work

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "../sources/hsla/numeric_array.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "test";
+    numeric_array color1{150, 150, 255, 2500};
+    cout << color1.to_string() << endl;
     return 0;
 }
