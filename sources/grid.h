29e632c01a170d71f83ee4f00209ee8d03994778
Oscar Hultén <oshulten@gmail.com>
2024-10-31 13:35:00 +0100

    feat: Grid - get minmax coords from all screen corners

--- sources/grid.h
+++ sources/grid.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <array>
+#include <ranges>
+#include <algorithm>
 #include <raylib.h>
 #include "RLCamera.h"
 
@@ -15,8 +18,16 @@ public:
         auto c = cam.cam();
         auto v = Vector2{0, 0};
 
-        auto minCoords = GetScreenToWorld2D({0, 0}, cam.cam());
-        auto maxCoords = GetScreenToWorld2D({static_cast<float>(GetScreenWidth()), static_cast<float>(GetScreenHeight())}, cam.cam());
+        auto topLeft = GetScreenToWorld2D({0, 0}, cam.cam());
+        auto topRight = GetScreenToWorld2D({static_cast<float>(GetScreenWidth()), 0}, cam.cam());
+        auto bottomRight = GetScreenToWorld2D({static_cast<float>(GetScreenWidth()), static_cast<float>(GetScreenHeight())}, cam.cam());
+        auto bottomLeft = GetScreenToWorld2D({0, static_cast<float>(GetScreenHeight())}, cam.cam());
+        std::array<float, 4> xCoords{topLeft.x, topRight.x, bottomRight.x, bottomLeft.x};
+        std::array<float, 4> yCoords{topLeft.y, topRight.y, bottomRight.y, bottomLeft.y};
+        std::ranges::sort(xCoords);
+        std::ranges::sort(yCoords);
+        Vector2 minCoords{xCoords[0], yCoords[0]};
+        Vector2 maxCoords{xCoords[3], yCoords[3]};
 
         for (int i = 0; i <= extension; i++)
         {
